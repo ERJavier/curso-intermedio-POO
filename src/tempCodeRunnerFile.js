c102ea6b73355fc225f38056fca9a709ae6e7ea0
Javier Cepeda <erjavierc@gmail.com>
2022-11-04 07:58:02 -0400

    objects

--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -3,6 +3,8 @@ const sample = {
     age: 18,
     approvedCourses: ["class 1"],
     addCourse(newCourse){
+        console.log("this", this)
+        console.log("this.approvedCourse", this.approvedCourses)
         this.approvedCourses.push(newCourse);
     }
 }
@@ -10,3 +12,5 @@ const sample = {
 console.log(Object.keys(sample))
 console.log(Object.getOwnPropertyNames(sample))
 console.log(Object.entries(sample))
+
+sample.addCourse("class 2")
