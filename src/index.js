673f936ca401998ab33ab24d9af21f8d6e669b5b
Javier Cepeda <erjavierc@gmail.com>
2022-11-09 08:46:16 -0500

    json parse & stringify

--- src/index.js
+++ src/index.js
@@ -34,17 +34,24 @@ const obj1 = {
     c: {
         d: "d",
         e: "e"
+    },
+    editA() {
+        this.a = 'string'
     }
-};
-
+ };
 
 const obj2 = {}
 for (prop in obj1) {
     obj2[prop] = obj1[prop] 
-}
-
+};
 
 const obj3 = Object.assign({}, obj1);
 
 const obj4 = Object.create(obj1);
 
+
+// JSON.parse & JSON.stringify
+
+const stringifiedComplexObj = JSON.stringify(obj1);
+
+const obj5 = JSON.parse(stringifiedComplexObj);
